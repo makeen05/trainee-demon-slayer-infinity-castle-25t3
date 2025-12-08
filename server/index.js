f9675c106d99d84ed94e35fd1d4e9c24b7a4f57e
April <z5591917@ad.unsw.edu.au>
2025-12-09 00:13:35 +1100

    database setup

--- server/index.js
+++ server/index.js
@@ -45,5 +45,5 @@ app.post('/api/test', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running port ${PORT}`);
 });
