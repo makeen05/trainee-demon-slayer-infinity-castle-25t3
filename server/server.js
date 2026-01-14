e325399c64204bbc7603273eeb9799f70cc4600f
Makeen Alaeddin <makeenalaeddin@student.unsw.edu.au>
2026-01-14 14:25:25 +1100

    update vercel url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://trainee-demon-slayer-infinity-castl.vercel.app/', 'http://localhost:5173'],
+  origin: ['https://trainee-demon-slayer-infinity-castl.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
